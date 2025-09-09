93a224028ffbbf10b763fefdaa8e989a5fc27d23
hanhoon <hanhoon12@naver.com>
2025-09-09 17:51:16 +0900

    UpdateTimestamp

--- src/main/java/root/entity/plm/PlmContext.java
+++ src/main/java/root/entity/plm/PlmContext.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class PlmContext {
@@ -13,6 +16,8 @@ public class PlmContext {
     public int cnt;
     public int leftword;
     public int rightword;
+    @UpdateTimestamp
+    LocalDateTime updatedDate;
 
     public int getCnt() {
         return cnt;
