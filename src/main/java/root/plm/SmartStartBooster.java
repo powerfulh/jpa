572a9b880061b282a18a8adb211f843ebe96ac51
hanhoon <hanhoon12@naver.com>
2025-09-17 14:54:46 +0900

    #21

--- src/main/java/root/plm/SmartStartBooster.java
+++ src/main/java/root/plm/SmartStartBooster.java
@@ -13,6 +13,7 @@ public class SmartStartBooster {
 
     public Toke rightContext(Toke target, Toke left, List<PlmContext> contextList) {
         target.rightContext = contextList.stream().filter(StaticUtil.getContextFinder(left.getN(), target.getN())).mapToInt(item -> left.isRightSpace() ? item.space : item.cnt).sum();
+        if(left.getType().equals("무엇") && target.getType().equals("조사")) target.rightContext++;
         return target;
     }
 }
