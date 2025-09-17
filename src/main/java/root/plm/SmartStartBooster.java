8e98893145db746bc105cc824e11316c378b0913
powerful-room <hanhoon12@naver.com>
2025-09-17 21:49:43 +0900

    #22

--- src/main/java/root/plm/SmartStartBooster.java
+++ src/main/java/root/plm/SmartStartBooster.java
@@ -14,6 +14,7 @@ public class SmartStartBooster {
     public Toke rightContext(Toke target, Toke left, List<PlmContext> contextList) {
         target.rightContext = contextList.stream().filter(StaticUtil.getContextFinder(left.getN(), target.getN())).mapToInt(item -> left.isRightSpace() ? item.space : item.cnt).sum();
         if(left.getType().equals("무엇") && target.getType().equals("조사")) target.rightContext++;
+        if(left.isRightSpace() && target.getType().equals("어미")) target.rightContext--;
         return target;
     }
 }
