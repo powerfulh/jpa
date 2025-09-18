c055e9f7817dcaf0511401b428e3e947eb0f4d07
powerful-room <hanhoon12@naver.com>
2025-09-18 22:39:00 +0900

    #21

--- src/main/java/root/plm/SmartStartBooster.java
+++ src/main/java/root/plm/SmartStartBooster.java
@@ -14,9 +14,9 @@ public class SmartStartBooster {
     public Toke rightContext(Toke target, Toke left, List<PlmContext> contextList) {
         target.rightContext = contextList.stream().filter(StaticUtil.getContextFinder(left.getN(), target.getN())).mapToInt(item -> left.isRightSpace() ? item.space : item.cnt).sum();
         target.rightContext *= left.getWord().length() + target.getWord().length();
-        if(left.getType().equals("무엇") && target.getType().equals("조사")) target.rightContext++;
         if(left.getType().equals("0") && target.getType().equals("조사") && !target.getN().equals(191)) target.rightContext--;
         if(left.isRightSpace() && target.getType().equals("어미")) target.rightContext--;
+        if(!left.isRightSpace() && (left.getType().equals("무엇") || left.getType().equals("대명사")) && target.getType().equals("조사")) target.rightContext++;
         if(!left.isRightSpace() && !left.getN().equals(StaticUtil.opener) && target.getType().equals("감탄사")) target.rightContext--;
         return target;
     }
