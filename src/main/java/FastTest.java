9f74ce3e7618ccbc040c0c9d26e01f6a761d9f5c
hanhoon <hanhoon12@naver.com>
2025-09-25 13:31:55 +0900

    Pre-code

--- src/main/java/FastTest.java
+++ src/main/java/FastTest.java
@@ -8,7 +8,7 @@ public class FastTest {
     final int complete = 4; // ㄴ
     final int going = 8; // ㄹ
     final int respect = 17; // ㅂ
-    final int past = 20; // ㅂ
+    final int past = 20; // ㅆ
 
     char addFooter(char target, int footerIndex) {
         return (char) (target + footerIndex);
@@ -29,6 +29,8 @@ public class FastTest {
                 case 20: // ㅣ
                     return 2;
             }
+        } else if(footer == 17) {
+            if(jungIndex != 8) return 3; // 8: 'ㅗ' 는 존나 규칙이 없음 (고운 좁은 고와 좁아)
         }
         return 0;
     }
@@ -36,6 +38,9 @@ public class FastTest {
         int SIndex = target - SBase;
         int choIndex = SIndex / (21 * 28); // 초성 인덱스
         return (char) (SBase + (choIndex * 21 + motherIndex) * 28);
+    }
+    char removeFooter(char target) {
+        return (char) (target - ((target - SBase) % 28));
     }
     List<HelpResult> help(char target) {
         List<HelpResult> list = new ArrayList<>();
@@ -60,17 +65,20 @@ public class FastTest {
                 list.add(new HelpResult(309, String.valueOf(addFooter(target, complete))));
                 list.add(new HelpResult(215, addFooter(target, complete) + "다"));
                 break;
+            case 3:
+                list.add(new HelpResult(83, removeFooter(target) + "운데"));
+                list.add(new HelpResult(105, removeFooter(target) + "움"));
+                list.add(new HelpResult(184, removeFooter(target) + "워"));
+                list.add(new HelpResult(309, removeFooter(target) + "운"));
+                list.add(new HelpResult(50, removeFooter(target) + "워서"));
+                break;
         }
         return list;
     }
     public static void main(String[] args) {
         var comp = new FastTest();
-        char c0 = '가';
-        char c1 = '서';
-        char c2 = '키';
-        System.out.println(comp.help(c0).stream().map(HelpResult::word).toList());
-        System.out.println(comp.help(c1).stream().map(HelpResult::word).toList());
-        System.out.println(comp.help(c2).stream().map(HelpResult::word).toList());
+        var c = List.of('가', '서', '키', '돕', '깝', '눕');
+        c.forEach(item -> System.out.println(comp.help(item).stream().map(HelpResult::word).toList()));
     }
 }
 
